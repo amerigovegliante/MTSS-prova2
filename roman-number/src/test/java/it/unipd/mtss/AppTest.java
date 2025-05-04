d86275fb8d198093d87c6fb7b9129d7975a79b6c
amerigovegliante <veglianteamerigo583@gmail.com>
2025-05-04 18:39:26 +0200

    config fix junit

--- roman-number/src/test/java/it/unipd/mtss/AppTest.java
+++ roman-number/src/test/java/it/unipd/mtss/AppTest.java
@@ -1,20 +1,14 @@
 package it.unipd.mtss;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void printTest()
     {
-        assertTrue( true );
+        fail();
     }
 }
