590e4c3984af2f5609a9fc7a5ffc6c6f3ab685fe
Giulia2424 <giuliarom4@gmail.com>
2025-05-06 19:04:31 +0200

    Convertire fino al 10, close #4

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,17 +7,19 @@ public class IntegerToRoman {
             for (int i=0;i<number;i++){
                 roman=roman+"I";
             }
-        }
-        else if(number <= 6)
-        {
-            if(number == 4)
+        }else if(number <= 8){
+            if(number == 4){
                 roman = "IV";
-            if(number == 5)
-                roman = "V";
-            if(number == 6)
-                roman = "VI";
-        }
+            }else{
+                roman="V";
+                for (int i=0;i<number-5;i++){
+                roman=roman+"I";
+               }
+            }  
+        }else if (number<=10) {
+            if(number==9){roman="IX";}
+            if(number==10){roman="X";}
+        } 
         return roman;
     }
 }
-
